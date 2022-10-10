e55c05be6d2438a94796d35d529d5a4938c4943f
Bhagwati Prasad <bhagwatijoshi325@gmail.com>
2022-10-10 12:23:41 +0530

    query, route params, request Bodies and DTO , Post Requests , handeling routes, generate a controller, generate a nest module done

---
+++ nestjs-crash-course-2022/src/users/controllers/users/users.controller.ts
@@ -0,0 +1,59 @@
+import { Body, Controller, Get, Param, Post, Query, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateUserDto } from 'src/users/dtos/CreateUserDto.dto';
+
+// 'users' below is the route on which controller is registered
+@Controller('users')
+export class UsersController {
+    // @Get('')
+    // getUsers() {
+    //     return { usrname: 'Anson', email: 'email@e.c' };
+    // }
+
+    @Get('posts')
+    getUsersPost() {
+        return [
+            {
+                usrname: 'Anson',
+                email: 'email@e.c',
+                posts: [
+                    {
+                        id: 1,
+                    },
+                ],
+            },
+        ];
+    }
+
+    //   @Post()
+    //   createUser(@Req() request: Request , @Res() response: Response) {
+    //     console.log(request.body);
+    //     response.send("created");
+    //   }
+
+    @Post('create')
+    createUser(@Body() userData: CreateUserDto) {
+        console.log(userData);
+        return {};
+    }
+
+
+    //   @Get(':id')
+    //   getUserById(@Req() request: Request , @Res() response: Response ){
+    //     console.log(request.params)
+    //   }
+
+    @Get(':id/:postId')
+    getUserById(@Param('id') id:string , @Param('postId') postId: string){
+      console.log(id , postId);
+      return {id};
+    }
+
+    @Get()
+    getUsers(@Query('sortBy') sortBy: string) {
+        console.log(sortBy);
+        return {sortBy };
+    }
+
+
+}
